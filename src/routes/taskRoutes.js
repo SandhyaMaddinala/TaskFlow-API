8d358db82e921c1c6fa72945fa61183e2ab5621d
SandhyaMaddinala <sandhyamaddinala812@gmail.com>
2026-06-29 11:10:10 +0530

    Create taskRoutes.js

---
+++ src/routes/taskRoutes.js
@@ -0,0 +1,16 @@
+import { Router } from 'express';
+import {
+getTasks,
+createTask,
+updateTask,
+deleteTask
+} from '../controllers/taskController.js';
+const router = Router();
+router.route('/')
+.get(getTasks)
+.post(createTask);
+router.route('/:id')
+.get(getTasks) // Implementation omitted for brevity
+.put(updateTask)
+.delete(deleteTask);
+export default router;
